84575c88ed24710e4c2c3ff4437eca07188f7dfa
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-09-16 16:44:13 +0800

    Update BiteClass1ApplicationTests.java

--- biteClass1/src/test/java/com/frank/biteclass1/BiteClass1ApplicationTests.java
+++ biteClass1/src/test/java/com/frank/biteclass1/BiteClass1ApplicationTests.java
@@ -1,16 +1,19 @@
 package com.frank.biteclass1;
 
+import com.frank.biteclass1.domain.User;
+import com.frank.biteclass1.service.IUserService;
+import com.frank.biteclass1.service.impl.UserServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BiteClass1ApplicationTests {
-
+    @Autowired
+    IUserService iUserService;
     @Test
     void contextLoads() {
-
-
-
+        System.out.println(iUserService.check(new User(1,"frank","666")));
     }
 
 }
