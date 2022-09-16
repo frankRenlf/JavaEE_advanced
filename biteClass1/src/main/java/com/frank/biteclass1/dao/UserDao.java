c36c69d212e66e207bda8c38683eb304dc15c237
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-09-16 10:43:51 +0800

    update

---
+++ biteClass1/src/main/java/com/frank/biteclass1/dao/UserDao.java
@@ -0,0 +1,16 @@
+package com.frank.biteclass1.dao;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : biteClass1
+ * @Package : com.frank.biteclass1.dao
+ * @createTime : 2022/9/16 10:43
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class UserDao {
+}
--- biteClass1/src/main/java/com/frank/biteclass1/domain/User.java
+++ biteClass1/src/main/java/com/frank/biteclass1/domain/User.java
@@ -20,42 +20,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
+    private Integer id;
+    private String username;
+    private String password;
 
-    Integer id;
-    String name;
-    String password;
-
-    public Integer getId() {
-        return id;
-    }
-
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
+    public User(String username, String password) {
+        this.username = username;
         this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", password='" + password + '\'' +
-                '}';
     }
 }
