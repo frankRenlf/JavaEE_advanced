1fa7d8cf1d2e49e20923d7bb559f1891427cdf0d
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-09-29 16:03:17 +0800

    1

---
+++ spring-1/src/main/java/App.java
@@ -0,0 +1,17 @@
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : spring-1
+ * @Package : PACKAGE_NAME
+ * @createTime : 2022/9/29 15:59
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
