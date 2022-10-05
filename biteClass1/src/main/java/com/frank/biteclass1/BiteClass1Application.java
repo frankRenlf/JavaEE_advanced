263d0b038f87ce0e28b8f4606c9645c09c03f845
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-10-05 12:45:22 +0800

    update

--- biteClass1/src/main/java/com/frank/biteclass1/BiteClass1Application.java
+++ biteClass1/src/main/java/com/frank/biteclass1/BiteClass1Application.java
@@ -8,7 +8,7 @@ public class BiteClass1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(BiteClass1Application.class, args);
-        System.out.println(333);
+        System.out.println(555);
     }
 
 }
