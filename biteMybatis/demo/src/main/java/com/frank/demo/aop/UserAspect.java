3ade7203491e5691c08ecc6fed275d57596d9a9c
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-10-08 14:25:59 +0800

    update

--- biteMybatis/demo/src/main/java/com/frank/demo/aop/UserAspect.java
+++ biteMybatis/demo/src/main/java/com/frank/demo/aop/UserAspect.java
@@ -1,7 +1,7 @@
 package com.frank.demo.aop;
 
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 /**
@@ -24,6 +24,42 @@ public class UserAspect {
     public void pointcut() {
     }
 
+    @Before("pointcut()")
+    public void doBefore() {
+        System.out.println("执⾏ Before ⽅法");
+    }
 
+    // 后置通知
+    @After("pointcut()")
+    public void doAfter() {
+        System.out.println("执⾏ After ⽅法");
+    }
 
+    // return 之前通知
+    @AfterReturning("pointcut()")
+    public void doAfterReturning() {
+        System.out.println("执⾏ AfterReturning ⽅法");
+    }
+
+    // 抛出异常之前通知
+    @AfterThrowing("pointcut()")
+    public void doAfterThrowing() {
+        System.out.println("执⾏ doAfterThrowing ⽅法");
+    }
+
+    // 添加环绕通知
+    @Around("pointcut()")
+    public Object doAround(ProceedingJoinPoint joinPoint) {
+        Object obj = null;
+        System.out.println("Around ⽅法开始执⾏");
+        try {
+// 执⾏拦截⽅法
+            obj = joinPoint.proceed();
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+        System.out.println("Around ⽅法结束执⾏");
+        return obj;
+    }
 }
+
---
+++ biteMybatis/demo/src/test/java/com/frank/demo/controller/UserControllerTest.java
@@ -0,0 +1,38 @@
+package com.frank.demo.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : biteClass1
+ * @Package : com.frank.demo.controller
+ * @createTime : 2022/10/8 14:24
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+@SpringBootTest
+class UserControllerTest {
+
+    @Autowired
+    private UserController userController;
+    @Test
+    void getAll1() {
+        System.out.println(userController.getAll1());
+    }
+
+    @Test
+    void getAll2() {
+    }
+
+    @Test
+    void getById() {
+    }
+}
