bbcff9d10aa76157952a3177bee5a080716e2fdd
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-11-03 17:41:31 +0800

    update

---
+++ spring-1/src/main/java/User.java
@@ -0,0 +1,17 @@
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : spring-1
+ * @Package : PACKAGE_NAME
+ * @createTime : 2022/11/3 17:40
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class User {
+    public void sayHi(){
+        System.out.println("Hi frank");
+    }
+}
