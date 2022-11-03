375b3fa8bfa25a2fc62cd92d69b84dc757e8b51a
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-11-03 18:30:21 +0800

    1

--- spring-1/src/main/java/Application.java
+++ spring-1/src/main/java/Application.java
@@ -1,3 +1,6 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -12,6 +15,8 @@
  */
 public class Application {
     public static void main(String[] args) {
-
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
+        User user = (User) context.getBean("user1", User.class);
+        user.sayHi();
     }
 }
