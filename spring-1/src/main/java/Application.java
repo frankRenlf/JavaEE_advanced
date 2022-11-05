60366b58fa57a66a40890052f6dca252c6fd2df1
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-11-05 17:45:14 +0800

    update

--- spring-1/src/main/java/Application.java
+++ spring-1/src/main/java/Application.java
@@ -2,6 +2,11 @@ import com.domain.User;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.concurrent.*;
+
+import static java.lang.Thread.sleep;
+import static java.time.LocalTime.now;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -15,7 +20,30 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * @Description :
  */
 public class Application {
+    private static void createThreadPool() {
+        ExecutorService executorService = new ThreadPoolExecutor(2, 10,
+                1, TimeUnit.MINUTES, new ArrayBlockingQueue<>(5, true),
+                Executors.defaultThreadFactory(), new ThreadPoolExecutor.AbortPolicy());
+        for (int i = 0; i < 10; i++) {
+            final int index = i;
+            executorService.execute(() -> {
+                // 获取线程名称,默认格式:pool-1-thread-1
+                System.out.println(now() + " " + Thread.currentThread().getName() + " " + index);
+                // 等待2秒
+                try {
+                    sleep(2000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+    }
+
     public static void main(String[] args) {
+        createThreadPool();
+    }
+
+    public static void main1(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
         User user1 = context.getBean("user1", User.class);
 //        user1.sayHi();
