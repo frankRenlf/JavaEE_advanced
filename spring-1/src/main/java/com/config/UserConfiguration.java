4e6842a65279f7854701ef26ef28c6be837c30cd
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-11-07 20:40:15 +0800

    Create UserConfiguration.java

---
+++ spring-1/src/main/java/com/config/UserConfiguration.java
@@ -0,0 +1,19 @@
+package com.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : spring-1
+ * @Package : com.config
+ * @createTime : 2022/11/7 20:24
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+@Configuration
+public class UserConfiguration {
+}
