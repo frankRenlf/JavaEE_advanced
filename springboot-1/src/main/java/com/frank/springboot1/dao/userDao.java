23d1226dda31b43f74e6e0ce3389ea62ddf2d34d
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-11-19 14:38:44 +0800

    update

---
+++ springboot-1/src/main/java/com/frank/springboot1/dao/userDao.java
@@ -0,0 +1,16 @@
+package com.frank.springboot1.dao;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : springboot-1
+ * @Package : com.frank.springboot1.dao
+ * @createTime : 2022/11/19 14:38
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class userDao {
+}
