11a5a79667bab691f3e6032de49e8fe4282ed4d8
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-11-26 16:12:51 +0800

    Update UserController.java

--- springboot-1/src/main/java/com/frank/springboot1/controller/UserController.java
+++ springboot-1/src/main/java/com/frank/springboot1/controller/UserController.java
@@ -7,11 +7,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
+import java.util.UUID;
 
 /**
  * Created with IntelliJ IDEA.
@@ -59,6 +64,18 @@ public class UserController {
     @PostMapping("/login2")
     public Result testLogin2(@RequestBody User user) {
         return new Result(true, user, "success");
+    }
+
+    @PostMapping("/file")
+    public String param9(String name, @RequestPart("myfile") MultipartFile file) throws IOException {
+        // 获取⽂件后缀名
+        String fileName = Objects.requireNonNull(file.getOriginalFilename()).substring(file.getOriginalFilename().lastIndexOf("."));
+        // ⽂件保存地址
+        String filePath = ClassUtils.getDefaultClassLoader().getResource("static").getPath() +
+                "/" + UUID.randomUUID() + fileName;
+        // 保存⽂件
+        file.transferTo(new File(filePath));
+        return filePath + " 上传成功.";
     }
 
 }
