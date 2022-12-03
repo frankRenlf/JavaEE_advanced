13008e5f534c068a4f7b87a4d6af8776956656b3
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-03 10:37:04 +0800

    update

--- ademo/src/test/java/com/example/ademo/mapper/ArticleMapperTest.java
+++ ademo/src/test/java/com/example/ademo/mapper/ArticleMapperTest.java
@@ -68,10 +68,10 @@ class ArticleMapperTest {
     void mySelectByParams() {
         Article article = new Article();
         article.setTitle("c");
-        Date s = new Date();
-        Date e = new Date();
-        article.setCreateTime();
-        article.setUpdateTime();
+        Date s = new Date("Thu Oct 05 16:43:50 CST 2022");
+        Date e = new Date("Thu Oct 22 16:43:51 CST 2022");
+        article.setCreateTime(s);
+        article.setUpdateTime(e);
         System.out.println(articleMapper.mySelectByParams(article));
     }
 }
