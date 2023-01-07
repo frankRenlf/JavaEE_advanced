fd78ceebfad3d1e54d64e1050c15c39604f7f49c
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-01-07 12:46:00 +0800

    add

---
+++ ademo/src/main/java/com/example/ademo/aop/LoginAOP.java
@@ -0,0 +1,30 @@
+package com.example.ademo.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : ademo
+ * @Package : com.example.ademo.aop
+ * @createTime : 2023/1/7 12:43
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+@Component
+@Aspect
+public class LoginAOP {
+    @Pointcut("execution(com.example.ademo.controller.UserController.*(..))")
+    public void pointcut(){}
+
+    @Before("pointcut()")
+    public void before(){
+        System.out.println("---------before---------");
+    }
+}
--- ademo/src/main/java/com/example/ademo/controller/utils/LoginConfig.java
+++ ademo/src/main/java/com/example/ademo/controller/utils/LoginConfig.java
@@ -20,7 +20,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class LoginConfig implements WebMvcConfigurer {
-    
+
     @Autowired
     private LoginInterceptor loginInterceptor;
 
